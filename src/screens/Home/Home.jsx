38296df7af6259c1344ec1accd5e23a6064d48d7
noctera <noctera.dev@gmail.com>
2021-11-19 23:34:49 +0100

    feat: added little hero section

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+import Button from "../../Components/Button/Button.jsx";
+
 import "./Home.scss";
 
 const Home = () => {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="hero-section">
+        <div className="hero-inner">
+          <h1>Never was learning easier</h1>
+          <p>Vocascan - A highly configurable vocabulary trainer</p>
+          <div className="button-wrapper">
+            <Button block uppercase disabled={true}>
+              {"Start Learning"}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
