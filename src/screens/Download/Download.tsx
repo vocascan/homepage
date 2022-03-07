a6c0c25e7d5cd08eff930e8d1b8f7af12f0304aa
noctera <noctera.dev@gmail.com>
2022-03-07 19:14:38 +0100

    fix: wip for download page

--- src/screens/Download/Download.tsx
+++ src/screens/Download/Download.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./Download.scss";
 
 const Download = () => {
-  return <div className="download">Download</div>;
+  return (
+    <div className="w-full h-screen bg-background flex justify-center items-center">
+      <h1 className="uppercase text-white text-2xl">Work in progress...</h1>
+    </div>
+  );
 };
 
 export default Download;
